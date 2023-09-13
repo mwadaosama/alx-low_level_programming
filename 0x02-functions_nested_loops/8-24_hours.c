581556a44958b58c3ba16a9445d80fb1dd9e710a
mwadaosama <mememahjoup@gmail.com>
2023-09-13 09:44:14 -0700

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  ]* jack_bauer- Prints every minute of the day of Jack Bauer
+* jack_bauer- Prints every minute of the day of Jack Bauer
 * Return: ...
 */
 void jack_bauer(void)
