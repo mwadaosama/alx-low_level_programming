f4c69b342034db73e1058edbb0535aed1e052e59
mwadaosama <mememahjoup@gmail.com>
2023-09-13 11:33:04 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - Adds tow integers.
+* @num1: The first integer to be added.
+* @num2: The second integer to be added.
+* Return: Integer value
+*/
+int add(int num1, int num2)
+{
+return (num1 + num2);
+
+}
