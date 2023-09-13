e5f47897da80d258d87dec00997d422f552f650f
mwadaosama <mememahjoup@gmail.com>
2023-09-12 17:56:40 -0700

    3-islower

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-    int r;
 
-    r = _islower('H');
-    putchar(r + '0');
-    r = _islower('o');
-    putchar(r + '0');
-    r = _islower(108);
-    putchar(r + '0');
-    putchar('\n');
-    return (0);
+int r;
+r = _islower('H');
+putchar(r + '0');
+r = _islower('o');
+putchar(r + '0');
+r = _islower(108);
+putchar(r + '0');
+putchar('\n');
+return (0);
 }
